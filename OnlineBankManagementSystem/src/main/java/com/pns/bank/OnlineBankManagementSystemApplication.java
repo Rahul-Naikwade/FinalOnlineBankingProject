084dcd5b1559dd572a5d4f70b90bb94fdc40fc72
Ganesh-gavali <gavaliganesh37@gmail.com>
2023-06-27 20:00:53 +0530

    EmailConfiguration add

--- OnlineBankManagementSystem/src/main/java/com/pns/bank/OnlineBankManagementSystemApplication.java
+++ OnlineBankManagementSystem/src/main/java/com/pns/bank/OnlineBankManagementSystemApplication.java
@@ -1,17 +1,29 @@
 package com.pns.bank;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import com.pns.bank.config.EmailConfig;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 public class OnlineBankManagementSystemApplication {
+	@Autowired
+	private EmailConfig emailService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineBankManagementSystemApplication.class, args);
+
 	}
-	
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void sendEmail() {
+		emailService.sendEmail("gavaliganesh29@gmail.com", "this is body", "this is subject");
+	}
 
 }
